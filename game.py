80e6d627311b236f7572d836b63978f90ec5c39d
atishay <atishayjain759@gmail.com>
2020-10-01 17:40:12 +0530

    fixed the random value generator , shifted to main function so that new random no generate when user play again , and added capital Y in if condition

--- game.py
+++ game.py
@@ -1,18 +1,23 @@
 from random import randint
 
 
-random = randint(1, 10)
+
 
 def main():
+    random = randint(1, 10)
+    print("\t\tWelcome to guessing game")
     a = 3
     for i in range(3):
-        x = int(input("enter the no u want to guess btw 1 to 10:"))
+        x = int(input("guess the number btw 1 to 10:"))
         if x == random:
             print("\ngreat u have guess the right no\n\nCongratualtions you won the game!!!!!")
             break
-        else:
-            a = a - 1
-            print("wrong... try again", a, 'left')
+        elif x < random:
+            print("\nlower than guess value")
+        elif x > random:
+            print("greater than guess value")    
+        a = a - 1
+        print("wrong... try again", a, 'chance left')
 
     else:
         print("sry...!! no chance left")
@@ -21,7 +26,7 @@ def main():
 main()
 
 s = input("\nwant to play again for yes press y or no for n:")
-if s == 'y':
+if s == 'y' or s=='Y':
     main()
 else:
     print("thanks for giving u r tym!!!")
